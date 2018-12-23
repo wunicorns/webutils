ed25b0982f6418ba611fc236675f6bd114eae7ca
human caspell <caspell84@gmail.com>
2018-12-23 16:11:06 +0900

    first upload

--- static/js/web.forms.js
+++ static/js/web.forms.js
@@ -1,7 +1,20 @@
 (function($){
 
 
-    
+    $.fn.extend({
+        formValue:function(opt){
+              var data = $.extend({}, opt);
+              $(this).find('input,textarea,select').each(function(i, elem){
+                  var value = $(elem).val();
+  
+                  if (!!elem.name) {
+                      data[elem.name] = value;
+                  }
+  
+              });return data;
+          }
+      });
+  
 
 
 })(jQuery);
